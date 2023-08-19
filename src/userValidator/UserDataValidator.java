be7c8b13d9a27cb58ae61c64bb144afe6d921f42
peter <peterfomunung@yahoo.com>
2023-08-19 16:38:55 -0400

    Added changes

--- src/userValidator/UserDataValidator.java
+++ src/userValidator/UserDataValidator.java
@@ -3,37 +3,51 @@ package userValidator;
 import java.io.*;
 
 public class UserDataValidator {
-	public static void main(String[] args) {
-		
-		// Inside a try block, instantiate 3 separate objects: a 'reader' that reads the
-		// data inside user_data.txt, a 'validWriter' for
-		// writing to a file with all the valid data,
-		// and an errorWriter for writing to a file with all the faulty data.
-		// If this try block fails, it would have to be because of an error from
-		// instantiating these readers (e.g. file not found, I/O exception, etc.), so
-		// just print the error to the console
-		
-		
-			// Run a while loop that iterates through each line of the file
+    public static void main(String[] args) {
+        try {
+            // Instantiate readers and writers
+            BufferedReader reader = new BufferedReader(new FileReader("user_data.txt"));
+            BufferedWriter validWriter = new BufferedWriter(new FileWriter("valid_data.txt"));
+            BufferedWriter errorWriter = new BufferedWriter(new FileWriter("error_data.txt"));
 
+            // Run a while loop to iterate through each line of the file
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    // Split the line by comma and check for missing data
+                    String[] userData = line.split(",");
+                    if (userData.length != 3) {
+                        throw new Exception("Missing Data");
+                    }
 
-				// In another try / catch block (inside the while loop), do the following:
-				// 1. Use the .split() method on the current line on "," so we can get an array
-				// of the name, email, and age of each user
-				// 2. If the length of the array is different than 3, we know something is wrong
-				// so throw an exception with message "Missing Data"
-				// 3. Save each piece of data in the array to its own variable, making sure to
-				// trim them of whitespace with .trim() and casting them to the correct datatype
-				// (Email + Name are strings, Age is int)
-				// 4. If age is less than or equal to 0, throw an exception with message
-				// "Invalid Age"
-				// 5. If the line passed all of these checks, write the line with the
-				// validWriter!
-				// 6. In the catch block, write the faulty line with the errorWriter appended
-				// with the error message!
+                    // Extract data and trim whitespace
+                    String name = userData[0].trim();
+                    String email = userData[1].trim();
+                    int age = Integer.parseInt(userData[2].trim());
 
-		// After the last catch statement, use a finally statement to close out of all
-		// of your readers and writers. This will require another try/catch block ;)
-		
-	}
+                    // Check for invalid age
+                    if (age <= 0) {
+                        throw new Exception("Invalid Age");
+                    }
+
+                    // Write to appropriate file
+                    validWriter.write(line);
+                    validWriter.newLine();
+                } catch (Exception e) {
+                    // Write faulty line to error file
+                    errorWriter.write(line + " - " + e.getMessage());
+                    errorWriter.newLine();
+                }
+            }
+
+            // Close readers and writers
+            reader.close();
+            validWriter.close();
+            errorWriter.close();
+
+        } catch (IOException e) {
+            // Print error if file operation fails
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
 }
